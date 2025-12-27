98ef4d6936bb44e74933965f8c81564a27a1f47d
Origin990 <Origin990@outlook.com>
2025-12-27 19:30:00 +0800

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
+import './main.styl'
 
+import { analytics } from 'star-rail-vue'
+import App from './App.vue'
+import { loadDatabase } from './assets/scripts/database'
+import { hotkey } from './assets/scripts/hotkey'
+import { logCheck, updateCheck } from './assets/scripts/update'
+
+analytics('G-PW30Q3ZHX1', import.meta.env.MODE === 'development')
+
+createApp(App).mount('#app')
+
+hotkey()
+logCheck()
+loadDatabase().then(updateCheck)
